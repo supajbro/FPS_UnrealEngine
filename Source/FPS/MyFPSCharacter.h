6e8f6934aafaad973a43be661c55b1cb681f36b5
supajbro <supajbro@gmail.com>
2025-10-20 16:32:34 +1000

    Setup basic player movement

---
+++ Source/FPS/MyFPSCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FPSCharacter.h"
+#include "MyFPSCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_API AMyFPSCharacter : public AFPSCharacter
+{
+	GENERATED_BODY()
+	
+};
